41c968669ccd71e25ed7228d0615623eaa516ee5
OscarHealyATU <G00424324@atu.ie>
2025-11-17 03:35:22 +0000

    adding .env

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -59,32 +59,6 @@ app.get('/api/movie/:id', async (req, res) => {
   const movie = await Movie.findById(req.params.id);
   res.send(movie);
 });
-app.get('/api/movies', (req, res) => {
-  const myMovies = [
-    {
-      "Title": "Avengers: Infinity War (server)",
-      "Year": "2018",
-      "imdbID": "tt4154756",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Captain America: Civil War (server)",
-      "Year": "2016",
-      "imdbID": "tt3498820",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "World War Z (server)",
-      "Year": "2013",
-      "imdbID": "tt0816711",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    }
-  ]
-  res.json({ myArray: myMovies });
-})
 
 
 // app.post('/api/movies', (req, res) => {
